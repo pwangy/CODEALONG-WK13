bb17160f678e87acd8800855d2d08fb93aa5535d
Peggy <64912576+pwangy@users.noreply.github.com>
2020-11-11 14:42:14 +0100

    commit

--- components/Counter.js
+++ components/Counter.js
@@ -3,10 +3,6 @@ import { View, Text, StyleSheet, Button } from 'react-native'
 
 export default function Counter() {
   const [count, setCount] = useState(0)
-  const Button = styled.button `
-    background-color: blue;
-    color: white;
-    border-radius: 8px;`
 
   return (
     <View>
