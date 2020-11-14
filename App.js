0d64128fe53750d33245a55cfaf73cf66fd2292f
Peggy <64912576+pwangy@users.noreply.github.com>
2020-11-14 13:21:57 +0100

    code push

--- App.js
+++ App.js
@@ -1,13 +1,73 @@
-import React from 'react';
+import React, { useState} from 'react'
+import styled from 'styled-components/native'
+import { TouchableOpacity, Text } from 'react-native'
 
-import { Container } from './components/Container'
-import { Houses } from './components/Houses'
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`
+const TopContainer = styled.View`
+flex: 1;
+width: 100%;
+background-color: #222222;
+justify-content: center;
+align-items: center;
+`
 
-export default function App() {
+const TopHeader = styled.Text`
+  font-size: 48px;
+  color: #efefef;
+`
+
+const BottomContainer = styled.View`
+  flex: 3;
+  width: 100%;
+  background-color: #fff;
+  justify-content: center;
+  align-items: center;
+`
+
+const BottomText = styled.Text`
+  font-size: 32px;
+  color: #1f1f1f;
+`
+
+const App = () => {
+  fetch(URL)
+    .then((response) => {
+      if (!response.ok) {
+          // something is wrong
+          throw `Got error: ${response.status}`
+      }
+      return response.json()
+    })
+    .then((json) => {
+      // do something interesting with data
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+
+    const [count, setCount] = useState(0)
+    const onIncrement = () => {
+      setCount(count + 1)
+      console.log('I has incremented!')
+    }
+
   return (
     <Container>
-      <Houses />
+      <TopContainer>
+        <TopHeader>imma counter!</TopHeader>
+      </TopContainer>
+
+      <BottomContainer>
+        <TouchableOpacity onPress={onIncrement}>
+          <Text>Add +1</Text>
+        </TouchableOpacity>
+      </BottomContainer>
     </Container>
   )
 }
-// updating file to push branch
+
+export default App
